be6cd2d0f3de07212b9ad5645fd4ad5e10a53932
Jiri Bruchanov <jbruchanov@gmail.com>
2013-09-23 22:18:48 +0100

    First working run with new API

--- MyPlacesApp/src/com/scurab/android/myplaces/activity/MainActivity.java
+++ MyPlacesApp/src/com/scurab/android/myplaces/activity/MainActivity.java
@@ -1,96 +1,78 @@
 package com.scurab.android.myplaces.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import com.google.android.maps.MapView;
 import com.scurab.android.myplaces.R;
-import com.scurab.android.myplaces.interfaces.ActivityResultListener;
 import com.scurab.android.myplaces.presenter.BasePresenter;
 import com.scurab.android.myplaces.presenter.MainActivityPresenter15;
 import com.scurab.android.myplaces.widget.MapItemPanel;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
-
 public class MainActivity extends BaseMapActivity {
-	
-	private MapView mMapView;
-	private View mContentView;
-	private ImageButton mMyLocation;
-	private ProgressBar mProgressBar;
-	private MainActivityPresenter15 mPresenter;
-	private MapItemPanel mMapItemPanel;
 
-	
+    private View mContentView;
+    private ImageButton mMyLocation;
+    private ProgressBar mProgressBar;
+    private MainActivityPresenter15 mPresenter;
+    private MapItemPanel mMapItemPanel;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
-        
-       
-    	mPresenter = getActivityPresenter();        
+
+
+        mPresenter = getActivityPresenter();
     }
-    
-    protected MainActivityPresenter15 getActivityPresenter()
-    {
-    	return new MainActivityPresenter15(this);
+
+    protected MainActivityPresenter15 getActivityPresenter() {
+//        return new MainActivityPresenter15(this);
+        return null;
     }
-    
-    protected void init()
-    {
-    	setContentView(getContentView());
-    	mMapView = (MapView)findViewById(R.id.mapView);
-    	mMyLocation = (ImageButton)findViewById(R.id.ibMyLocation);
-    	mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
-    	mMapItemPanel = (MapItemPanel)findViewById(R.id.mapItemPanel);
+
+    protected void init() {
+        setContentView(getContentView());
+        mMyLocation = (ImageButton) findViewById(R.id.ibMyLocation);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mMapItemPanel = (MapItemPanel) findViewById(R.id.mapItemPanel);
     }
-    
-    protected int getContentViewResId()
-    {
-    	return R.layout.main;
+
+    protected int getContentViewResId() {
+        return R.layout.main;
     }
-    
+
     @Override
-	public View getContentView()
-    {
-    	if(mContentView == null)
-    		mContentView = View.inflate(this, getContentViewResId(), null);
-    	return mContentView;
+    public View getContentView() {
+        if (mContentView == null) {
+            mContentView = View.inflate(this, getContentViewResId(), null);
+        }
+        return mContentView;
     }
 
-	@Override
-	protected boolean isRouteDisplayed()
-	{
-		return false;
-	}
+    @Deprecated
+    public MapView getMapView() {
+        return null;
+    }
 
-	public MapView getMapView()
-	{
-		return mMapView;
-	}
+    public ImageButton getMyLocationButton() {
+        return mMyLocation;
+    }
 
-	public ImageButton getMyLocationButton()
-	{
-		return mMyLocation;
-	}
+    public ProgressBar getProgressBar() {
+        return mProgressBar;
+    }
 
-	public ProgressBar getProgressBar()
-	{
-		return mProgressBar;
-	}
+    @Override
+    public BasePresenter getPresenter() {
+        return mPresenter;
+    }
 
-	@Override
-	public BasePresenter getPresenter()
-	{
-		return mPresenter;
-	}
-	
-	public MapItemPanel getMapItemPanel()
-	{
-		return mMapItemPanel;
-	}
+    public MapItemPanel getMapItemPanel() {
+        return mMapItemPanel;
+    }
 
-	
+
 }
